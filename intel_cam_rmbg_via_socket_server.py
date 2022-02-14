269fc57ee224739bad38f4b46d4b8ba2598ebe33
tony19990828 <58881058+tony19990828@users.noreply.github.com>
2022-02-14 12:41:59 +0800

    Update intel_cam_rmbg_via_socket_server.py

--- intel_cam_rmbg_via_socket_server.py
+++ intel_cam_rmbg_via_socket_server.py
@@ -5,7 +5,7 @@ import sys
 from base64 import b64decode
 import cv2
 import json
-from simple_facerec import SimpleFacerec
+# from simple_facerec import SimpleFacerec
 
 #########################################################################
 # socket
@@ -14,7 +14,8 @@ PORT = 5050
 
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDRESS = (SERVER, PORT)
-FORMAT = "utf-8"
+FORMAT_LENGTH = "utf-8"
+FORMAT = "ascii"
 DISCONNECT_MSG = "!DISCONNECTED"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,33 +23,15 @@ server.bind(ADDRESS)
 #########################################################################
 #########################################################################
 # stream_fac_rec
-sfr = SimpleFacerec()
-sfr.load_encoding_images("me/")
-
-process_this_frame = True
+# sfr = SimpleFacerec()
+# sfr.load_encoding_images("me/")
+#
+# process_this_frame = True
 #########################################################################
 
 
 def stream_fac_rec(frame):
-    # print("hello")
-    bg_gray = frame[:480, :]
-    depth = frame[480:, :]
-
-    # if process_this_frame:
-    face_locations, face_names = sfr.detect_known_faces(bg_gray)
-    for face_loc, face_name in zip(face_locations, face_names):
-        # face_loc[ 24 372 168 228]
-        # face_loc[ (top right) ( bottom left)]
-        x1, y1, x2, y2 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
-        # dis = depth[x3, y3]
-        cv2.putText(bg_gray, face_name, (y2, x1 - 30), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 0, 0),
-                    2)
-
-        cv2.rectangle(bg_gray, (y1, x1), (y2, x2), (0, 0, 255), 3)
-        cv2.imshow('bg_gray', bg_gray)
-
-    # process_this_frame = not process_this_frame
-
+    cv2.imshow('stream', frame)
     key = cv2.waitKey(1)
 
 
@@ -57,22 +40,35 @@ def handle_client(conn, addr):
 
     connected = True
     while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
+        flage = False
+        msg_length = conn.recv(HEADER).decode(FORMAT_LENGTH)
         if msg_length:
-            # print(f"msg_length {msg_length}")
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
             len_msg = len(msg)
+            print(f"msg_length {msg_length}")
             if msg_length != len_msg:
+                print(f"not match {len_msg}")
+                remain_data_size = msg_length - len_msg
                 while int(msg_length) != int(len_msg):
-                    msg += conn.recv(msg_length).decode(FORMAT)
+                    msg_compensation = conn.recv(remain_data_size).decode(FORMAT)
+                    print(f"remain_data_size {remain_data_size}")
+                    # print(f"msg_compensation [len({len(msg_compensation)}){msg_compensation}]")
+                    msg += msg_compensation
                     len_msg = len(msg)
+                    remain_data_size = msg_length - len_msg
+
+                    if len_msg > msg_length:
+                        print("overload")
+                        print(msg)
+                        assert False
             if msg == DISCONNECT_MSG:
                 connected = False
+
             b64_r = json.loads(msg)
             JPEG_r = b64decode(b64_r["image"])
             frame = cv2.imdecode(np.frombuffer(JPEG_r, dtype=np.uint8), cv2.IMREAD_COLOR)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             stream_fac_rec(frame)
 
     conn.close()
@@ -84,7 +80,7 @@ def start():
     while True:
         conn, addr = server.accept()
         handle_client(conn, addr)
-        # thread = threading.Thread(target=handle_client, args=(conn, addr))
+        # thread = threading.Thread(target=handle_client, args=(conn, addr)\)
         # thread.start()
         # print(f"[ACTIVATE CONNECTING]{threading.active_count() - 1}")
 
