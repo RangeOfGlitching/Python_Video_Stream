d5bede82cacab27b6299e0c3f909d1a0cad336b3
RangeOfGlitching <58881058+RangeOfGlitching@users.noreply.github.com>
2022-03-25 13:19:27 +0800

    Update recording.py

--- intel 435i/recording.py
+++ intel 435i/recording.py
@@ -8,6 +8,7 @@ pipeline = rs.pipeline()
 config = rs.config()
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+# (1280,720)
 
 now = datetime.now()
 current_time = now.strftime("%H_%M_%S")
