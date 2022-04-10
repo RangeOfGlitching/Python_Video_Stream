af651b15e60471136cac0cab82208f34fe83324b
RangeOfGlitching <58881058+RangeOfGlitching@users.noreply.github.com>
2022-04-10 15:56:51 +0800

    Update intel_cam_rmbg_via_socket_server.py

--- intel 435i/intel_cam_rmbg_via_socket_server.py
+++ intel 435i/intel_cam_rmbg_via_socket_server.py
@@ -21,16 +21,6 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDRESS)
 
 
-#########################################################################
-#########################################################################
-# stream_fac_rec
-# sfr = SimpleFacerec()
-# sfr.load_encoding_images("me/")
-#
-# process_this_frame = True
-#########################################################################
-
-
 def stream_fac_rec(frame):
     cv2.imshow('stream', frame)
     key = cv2.waitKey(1)
@@ -38,17 +28,23 @@ def stream_fac_rec(frame):
 
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION]{addr} connected.")
-
     connected = True
     while connected:
         desireMsgLength = conn.recv(HEADER).decode(FORMAT)
+        current_desireMsgLength = len(desireMsgLength)
+        while current_desireMsgLength != HEADER:
+            remain_data_size = HEADER - current_desireMsgLength
+            msg_compensation = conn.recv(remain_data_size).decode(FORMAT)
+            desireMsgLength += msg_compensation
+            current_desireMsgLength = len(desireMsgLength)
+
         if desireMsgLength:
             desireMsgLength = int(desireMsgLength)
             msg = conn.recv(desireMsgLength).decode(FORMAT)
             realMshLength = len(msg)
             # print(f"msg_length {desireMsgLength}")
             # print(f"    not match {realMshLength}")
-            while desireMsgLength > realMshLength:
+            while desireMsgLength != realMshLength:
                 remain_data_size = desireMsgLength - realMshLength
                 msg_compensation = conn.recv(remain_data_size).decode(FORMAT)
                 # print(f"    remain_data_size {remain_data_size}")
@@ -56,18 +52,20 @@ def handle_client(conn, addr):
                 msg += msg_compensation
                 realMshLength = len(msg)
 
-                if realMshLength > desireMsgLength:
-                    print("overload")
-                    print(msg)
-                    assert False
             if msg == DISCONNECT_MSG:
                 connected = False
+            try:
+                b64_r = json.loads(msg)
+                JPEG_r = b64decode(b64_r["image"])
+            except json.decoder.JSONDecodeError as JSONDecodeError:
+                print(JSONDecodeError)
+                print(f"desireMsgLength {desireMsgLength}  realMshLength {realMshLength}")
+                print(b64_r["image"])
+                connected = False
+                continue
 
-            b64_r = json.loads(msg)
-            JPEG_r = b64decode(b64_r["image"])
-            frame = cv2.imdecode(np.frombuffer(JPEG_r, dtype=np.uint8), cv2.IMREAD_COLOR)
-            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            stream_fac_rec(frame)
+        frame = cv2.imdecode(np.frombuffer(JPEG_r, dtype=np.uint8), cv2.IMREAD_COLOR)
+        stream_fac_rec(frame)
 
     conn.close()
 
@@ -78,7 +76,7 @@ def start():
     while True:
         conn, addr = server.accept()
         handle_client(conn, addr)
-        # thread = threading.Thread(target=handle_client, args=(conn, addr)\)
+        # thread = threading.Thread(target=handle_client, args=(conn, addr))
         # thread.start()
         # print(f"[ACTIVATE CONNECTING]{threading.active_count() - 1}")
 
